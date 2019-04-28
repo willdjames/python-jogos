924fdd991d050009a03e54aa89dd650724781a19
Wiiliam D C Santana <willdjames@live.com>
2019-04-28 10:22:17 -0300

    Tratando o input

--- forca.py
+++ forca.py
@@ -6,6 +6,23 @@ def inicia_jogo():
     print("* Bem vindo ao jogo da Forca *")
     print("******************************")
 
+    palavra_secreta = "banana"
+
+    acertou = False
+    enforcou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Uma letra: ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("{} na posição {}".format(chute, index))
+
+            index += 1
+
     print("* Fim do jogo *")
 
 if(__name__ == "__main__"):
